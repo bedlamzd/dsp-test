64697f1ec476757365e6ea38c1d68df3e478a33d
bedlamzd <bedlamzd@gmail.com>
2020-07-19 16:38:27 +0300

    fixed error when saving to nonexistent directory

--- audio_magick.py
+++ audio_magick.py
@@ -1,14 +1,12 @@
 import pydub
 from pydub import AudioSegment
 import io
+import pathlib
 
 
-def save_voice(name, data, format='wav', bitrate='16k'):
-    audio = AudioSegment.from_file(io.BytesIO(data))
-    audio.export(name, format=format, bitrate=bitrate)
-
-
-def save_voice_to_wav(name, data, frame_rate=16000):
+def save_voice_to_wav(path, data, frame_rate=16000):
+    path = pathlib.Path(path)
+    path.parent.mkdir(parents=True, exist_ok=True)
     audio = AudioSegment.from_file(io.BytesIO(data))
     audio = audio.set_frame_rate(frame_rate)
-    audio.export(name, format='wav')
+    audio.export(path, format='wav')
