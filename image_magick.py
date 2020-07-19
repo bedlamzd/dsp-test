91861bf95ba2dfccc0c9a7f10130ff5e9485d163
bedlamzd <bedlamzd@gmail.com>
2020-07-20 02:04:00 +0300

    face detection files

---
+++ image_magick.py
@@ -0,0 +1,28 @@
+import pathlib
+import cv2
+from cv2.data import haarcascades
+
+face_cascade_name = f'{haarcascades}/haarcascade_frontalface_alt.xml'
+eyes_cascade_name = f'{haarcascades}/haarcascade_eye_tree_eyeglasses.xml'
+
+face_cascade = cv2.CascadeClassifier()
+eyes_cascade = cv2.CascadeClassifier()
+
+face_cascade.load(cv2.samples.findFile(face_cascade_name))
+eyes_cascade.load(cv2.samples.findFile(eyes_cascade_name))
+
+
+def contains_face(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.equalizeHist(img_gray)
+
+    faces = face_cascade.detectMultiScale(img_gray)
+    return any(faces)
+
+def read_img_from_bytearray(bytearray):
+    return cv2.imdecode(bytearray, cv2.IMREAD_COLOR)
+
+def save_img(path, img):
+    path = pathlib.Path(path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    cv2.imwrite(path, img)
