db5d89974ce14aa963595669e1748be5ab8f2ec1
bedlamzd <bedlamzd@gmail.com>
2020-07-19 19:12:05 +0300

    test saving files

--- audio_magick.py
+++ audio_magick.py
@@ -1,11 +1,13 @@
 from pydub import AudioSegment
 import io
 import pathlib
+import os
 
 
 def save_voice_to_wav(path, data, frame_rate=16000):
     path = pathlib.Path(path)
     path.parent.mkdir(parents=True, exist_ok=True)
+    print(os.listdir())
     audio = AudioSegment.from_file(io.BytesIO(data))
     audio = audio.set_frame_rate(frame_rate)
     audio.export(path, format='wav')
