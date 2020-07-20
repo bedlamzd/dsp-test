6705bbef0f58cdaa194f5db63754924938946f13
bedlamzd <bedlamzd@gmail.com>
2020-07-20 03:40:56 +0300

    bit of docs

--- BotUser.py
+++ BotUser.py
@@ -3,6 +3,10 @@ from image_magick import save_img
 
 
 class BotUser:
+    """
+    User class and database record
+    Stored in shelve database
+    """
     def __init__(self, id):
         self.id = str(id)
         self.records = []
@@ -12,27 +16,54 @@ class BotUser:
 
     @staticmethod
     def _audio_name(audio_id):
+        """
+        Convenience function to generate name from id
+        """
         return f'audio_message_{audio_id}.wav'
 
     def _audio_path(self, audio_id):
+        """
+        Convenience function to generate path from id
+        """
         return fr'.\{self.id}\{self._audio_name(audio_id)}'
 
     @staticmethod
     def _img_name(img_id):
+        """
+        Convenience function to generate name from id
+        """
         return f'face_img_{img_id}.png'
 
     def _image_path(self, img_id):
+        """
+        Convenience function to generate path from id
+        """
         return fr'.\{self.id}\{self._img_name(img_id)}'
 
     def get_audio(self, audio_id):
+        """
+        Get audio file by id
+        :param audio_id:
+        :return:
+        """
         return open(self._audio_path(audio_id), 'rb')
 
     def add_voice(self, voice):
+        """
+        Save audio file
+        :param voice:
+        :return:
+        """
         save_voice_to_wav(self._audio_path(self.record_id), voice)
         self.records.append(self._audio_name(self.record_id))
         self.record_id += 1
 
     def add_image(self, img):
+        """
+        Save image
+        :param img:
+        :return:
+        """
         save_img(self._image_path(self.img_id), img)
         self.imgs.append(self._img_name(self.img_id))
         self.img_id += 1
--- audio_magick.py
+++ audio_magick.py
@@ -4,6 +4,13 @@ import pathlib
 
 
 def save_voice_to_wav(path, data, frame_rate=16000):
+    """
+    Converts audio to wav format
+    :param path: where to save
+    :param data: audio file
+    :param frame_rate: in Hz
+    :return:
+    """
     path = pathlib.Path(path)
     path.parent.mkdir(parents=True, exist_ok=True)
     audio = AudioSegment.from_file(io.BytesIO(data))
